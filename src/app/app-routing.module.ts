32527af0bc6c50976eae631302a825405b5d2a2e
Tophe <bezedache29@gmail.com>
2022-01-23 21:07:12 +0100

    Creation module routing + config

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { FaceSnapListComponent } from "./face-snap-list/face-snap-list.component";
+
+// Routes de l'application
+const routes: Routes = [
+  { path: 'facesnaps', component: FaceSnapListComponent }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule {}
