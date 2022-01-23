88172ad11cfcdadb0eefe69e50b5b164fd2900fc
Tophe <bezedache29@gmail.com>
2022-01-23 12:23:53 +0100

    Utilisation d'un *ngFor pour boucler nos faceSnaps et les afficher

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,33 +7,61 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  mySnap!: FaceSnap;
-  secondSnap!: FaceSnap;
-  lastSnap!: FaceSnap;
+
+  faceSnaps!: FaceSnap[];
+
+  // mySnap!: FaceSnap;
+  // secondSnap!: FaceSnap;
+  // lastSnap!: FaceSnap;
 
   ngOnInit(): void {
-    this.mySnap = {
-      'title': 'Ripley',
-      'description': 'Mon pseudo depuis toujours',
-      'createdDate': new Date(),
-      'snaps': 0,
-      'imgUrl': 'https://figurinesalien.files.wordpress.com/2017/11/paul-jackson-alien-banana.jpg',
-      'location': 'Nantes'
-    }
-    this.secondSnap = {
-      'title': 'Bezedache',
-      'description': 'Mon deuxième pseudo',
-      'createdDate': new Date(),
-      'snaps': 2,
-      'imgUrl': 'https://logodix.com/logo/83317.png',
-      'location': 'Kerlouan'
-    }
-    this.lastSnap = {
-      'title': 'Morz',
-      'description': 'Mon premier pseudo',
-      'createdDate': new Date(),
-      'snaps':0,
-      'imgUrl': 'https://lemagdesanimaux.ouest-france.fr/images/dossiers/2020-06/mini/morse-095329-650-400.jpg'
-    }
+    this.faceSnaps = [
+      {
+        'title': 'Ripley',
+        'description': 'Mon pseudo depuis toujours',
+        'createdDate': new Date(),
+        'snaps': 0,
+        'imgUrl': 'https://figurinesalien.files.wordpress.com/2017/11/paul-jackson-alien-banana.jpg',
+        'location': 'Nantes'
+      },
+      {
+        'title': 'Bezedache',
+        'description': 'Mon deuxième pseudo',
+        'createdDate': new Date(),
+        'snaps': 2,
+        'imgUrl': 'https://logodix.com/logo/83317.png',
+        'location': 'Kerlouan'
+      },
+      {
+        'title': 'Morz',
+        'description': 'Mon premier pseudo',
+        'createdDate': new Date(),
+        'snaps':0,
+        'imgUrl': 'https://lemagdesanimaux.ouest-france.fr/images/dossiers/2020-06/mini/morse-095329-650-400.jpg'
+      }
+    ];
+    // this.mySnap = {
+    //   'title': 'Ripley',
+    //   'description': 'Mon pseudo depuis toujours',
+    //   'createdDate': new Date(),
+    //   'snaps': 0,
+    //   'imgUrl': 'https://figurinesalien.files.wordpress.com/2017/11/paul-jackson-alien-banana.jpg',
+    //   'location': 'Nantes'
+    // },
+    // this.secondSnap = {
+    //   'title': 'Bezedache',
+    //   'description': 'Mon deuxième pseudo',
+    //   'createdDate': new Date(),
+    //   'snaps': 2,
+    //   'imgUrl': 'https://logodix.com/logo/83317.png',
+    //   'location': 'Kerlouan'
+    // },
+    // this.lastSnap = {
+    //   'title': 'Morz',
+    //   'description': 'Mon premier pseudo',
+    //   'createdDate': new Date(),
+    //   'snaps':0,
+    //   'imgUrl': 'https://lemagdesanimaux.ouest-france.fr/images/dossiers/2020-06/mini/morse-095329-650-400.jpg'
+    // }
   }
 }
